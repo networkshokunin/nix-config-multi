05e77ac582a58bf169c9cc400f1740cc63755855
David Lim <david@networkshokunin.com>
2024-06-29 23:11:55 +0800

    Adding unifi

---
+++ hosts/mgt1/unifi.nix
@@ -0,0 +1,27 @@
+{ myvars, ... }:
+{
+  services.unifi.enable = true;
+  services.unifi.unifiPackage = pkgs.unifi;
+
+  services.nginx.virtualHosts."unifi.${myvars.domain}" = {
+    forceSSL = true;
+    useACMEHost = "unifi.${myvars.domain}";
+    locations."/".proxyPass = "http://localhost:8001";
+	  extraConfig = ''
+		  proxy_set_header Host $host;
+	  '';
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [ 
+                        8080  # Port for UAP to inform controller.
+                        #8880 # Port for HTTP portal redirect, if guest portal is enabled.
+                        8843  # Port for HTTPS portal redirect, ditto.
+                        #6789 # Port for UniFi mobile speed test.
+                      ];
+    allowedUDPPorts = [
+                        3478  # UDP port used for STUN.
+                        10001 # UDP port used for device discovery.
+                      ];
+  };
+}
